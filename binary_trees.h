f4b21aa92523e041b3361e3bcf805e6fc685b63d
de-jackson <jackyeyamu@gmail.com>
2022-02-14 15:06:46 +0300

    Solving tsk 3. Delete

--- binary_trees.h
+++ binary_trees.h
@@ -31,8 +31,10 @@ void binary_tree_print(const binary_tree_t *);
 
 /* Mains */
 
+/* Node creation and deletion */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
