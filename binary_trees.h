c4260b11c43c4145c4d3d35c150a4527d668e5c7
de-jackson <jackyeyamu@gmail.com>
2022-02-17 00:56:24 +0300

    Solving task 40. Heap - Sort

--- binary_trees.h
+++ binary_trees.h
@@ -97,5 +97,6 @@ bst_t *swap(bst_t *a, bst_t *b);
 char *convert(unsigned long int num, int base, int lowercase);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
 #endif /* BINARY_TREES_H */
