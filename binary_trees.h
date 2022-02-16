ff04ad17e2c41391fdda128b6f4a19086f9903b6
de-jackson <jackyeyamu@gmail.com>
2022-02-16 06:28:27 +0300

    Solving task 24. Is BST

--- binary_trees.h
+++ binary_trees.h
@@ -70,4 +70,7 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 
+/* Binary Tree to bst(Binary Search Tree) */
+int binary_tree_is_bst(const binary_tree_t *tree);
+
 #endif /* BINARY_TREES_H */
